c8709b98416beb3a306efa4ba73e230838f53ab9
Victor Rafael <contato@victorafael.com>
2017-10-22 21:15:49 -0200

    Updated jsons and editor now loads and outputs a new json

--- webApp/scripts/editor.js
+++ webApp/scripts/editor.js
@@ -3,7 +3,8 @@ var icons = [
 	"cleaning.png",
 	"health.png",
 	"keyboard.png",
-	"phrases.png"
+	"phrases.png",
+	"body.png"
 ];
 var current = 0;
 var formSample;
@@ -21,13 +22,19 @@ $(document).ready(function(){
 	interactableSample.appendTo("body").hide();
 	
 	$("#new").click(NewInteractable);
+	$("#open").click(Open);
+	$("#save").click(Save);
+	$("#load").click(Load);
+	$("#close").click(function(){
+		$("#filePanel").hide();
+	});
 });
 
 function NewInteractable(){
-	SpawnInteractable(formSample);
+	return SpawnInteractable(formSample);
 }
 function DuplicateInteractable(original){
-	SpawnInteractable(original);
+	return SpawnInteractable(original);
 }
 function SpawnInteractable(source){
 	var element = $(source).clone().appendTo("#formRoot").show();
@@ -36,6 +43,7 @@ function SpawnInteractable(source){
 		ApplyInteractableData(element);
 	});
 	var display = $(interactableSample).clone().appendTo("#interactableRoot").show().attr("index",current);
+	
 	ApplyInteractableData(element);
 	
 	element.find(".delete").click(function(){
@@ -52,6 +60,7 @@ function SpawnInteractable(source){
 		}
 	});
 	current++;
+	return element;
 }
 
 function ApplyInteractableData(el){
@@ -61,31 +70,84 @@ function ApplyInteractableData(el){
 		y: el.find(".y").val(),
 		width: el.find(".width").val(),
 		height: el.find(".height").val(),
-		background: el.find(".color").val(),
+		bg: el.find(".color").val(),
 		content: el.find(".txt").val(),
-		icon:el.find(".icon").val()
+		icon:el.find(".icon").val(),
+		type:el.find(".type").val(),
+		value:el.find(".data").val()
 	}
-	el.css({"background":data.background});
+	el.css({"background":data.bg});
 	el.find(".minify").text(data.content);
 	displayEl.css({
 		"top":data.y+"%",
 		"left":data.x+"%",
 		"width":data.width+"%",
 		"height":data.height+"%",
-		"background":data.background
+		"background":data.bg
 	});
 	if(data.icon.trim().length > 0){
 		displayEl.find("img").show().attr("src","icons/"+data.icon);
 	}else{
 		displayEl.find("img").hide();
 	}
-	var typeVal = el.find(".type").val();
-	if(typeVal == "text"){
-		displayEl.find("span").text(typeVal+":");
+	if(data.type == "text"){
+		displayEl.find("span").text(data.type+":");
 		el.find(".dataLine").hide();
 	}else{
-		displayEl.find("span").text(typeVal+":"+el.find(".data").val());
+		displayEl.find("span").text(data.type+":"+data.value);
 		el.find(".dataLine").show();
 	}
 	displayEl.find(".txt").text(data.content);
+}
+
+function Open(){
+	$("#textarea").text("");
+	$("#filePanel").show();
+}
+function Save(){
+	var allData = [];
+	$("#formRoot .interactableForm").each(function(){
+		var el = $(this);
+		allData.push({
+			x: el.find(".x").val(),
+			y: el.find(".y").val(),
+			width: el.find(".width").val(),
+			height: el.find(".height").val(),
+			bg: el.find(".color").val(),
+			content: el.find(".txt").val(),
+			icon:el.find(".icon").val(),
+			type:el.find(".type").val(),
+			value:el.find(".data").val()
+		});
+	});
+	
+	$("#textarea").val(JSON.stringify(allData));
+	$("#filePanel").show();
+}
+function ClearScreen(){
+	$("#formRoot").html("");
+	$("#interactableRoot").html("");
+}
+function Load(){
+	ClearScreen();
+	var txt = $("#textarea").val();
+	console.log(txt);
+	var allData = JSON.parse(txt);
+	for(var i = 0; i < allData.length; i++){
+		var data = allData[i];
+		var el = NewInteractable();
+		
+		el.find(".x").val(data.x);
+		el.find(".y").val(data.y);
+		el.find(".width").val(data.width);
+		el.find(".height").val(data.height);
+		el.find(".color").val(data.bg);
+		el.find(".txt").val(data.content);
+		el.find(".icon").val(data.icon);
+		el.find(".type").val(data.type);
+		el.find(".data").val(data.value);
+		
+		ApplyInteractableData(el);
+	}
+	$("#filePanel").hide();
 }
